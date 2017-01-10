5bffa8cf90286354fb456c9973561a72f6817c56
Cyre <kaai.cyre@gmail.com>
2017-01-10 21:05:09 +0000

    Image Viewer

---
+++ src/imageviewer/ui/swing/SwingImageDisplay.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageviewer.ui.swing;
+
+import imageviewer.model.Image;
+import imageviewer.ui.ImageDisplay;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Kaai
+ */
+public class SwingImageDisplay extends JPanel implements ImageDisplay {
+
+    private Image image;
+
+    public SwingImageDisplay() {
+        super(new BorderLayout());
+    }
+
+    @Override
+    public void display(Image image) {
+        this.image = image;
+        this.removeAll();
+        this.updateUI();
+        
+        this.add(imagePanel());
+    }
+
+    private Component imagePanel() {
+        return new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+                g.drawImage(bitmap(), 0, 0, this.getWidth(), this.getHeight(), null);
+                
+            }
+
+            private java.awt.Image bitmap() {
+                try {
+                    return ImageIO.read(new ByteArrayInputStream(image.bitmap()));
+                } catch (IOException ex) {
+                    return null;
+                }
+            }
+            
+        };
+    }
+
+    @Override
+    public Image currentImage() {
+        return image;
+    }
+    
+}
